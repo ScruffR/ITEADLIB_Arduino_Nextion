adb8d902c06fbad41bec43e40e1758f40885db66
Scruff.R <scruff.r@sbg.at>
2015-12-01 16:25:19 +0100

    wrong prototype

--- firmware/NexGauge.h
+++ firmware/NexGauge.h
@@ -33,7 +33,7 @@ public: /* methods */
     /**
      * @copydoc NexObject::NexObject(uint8_t pid, uint8_t cid, const char *name, void *value);
      */
-    NexGauge(uint8_t pid, uint8_t cid, const char *name);
+    NexGauge(uint8_t pid, uint8_t cid, const char *name, void *value = NULL);
 
     /**
      * Get the value of gauge.
