86d114f74159237fcffe9e51391dce162d99edb3
Scruff.R <scruff.r@sbg.at>
2015-12-06 03:27:15 +0100

    Move #defines from Nextion.h to NexConfig.h

--- firmware/Nextion.h
+++ firmware/Nextion.h
@@ -20,42 +20,6 @@
 
 #ifndef __NEXTION_H__
 #define __NEXTION_H__
-                              // applies to:
-#define NexBACKCOLOR    "bco"    // Text, Number, Button, Progressbar,        ,     , Gauge, Waveform, Slider,      ,         ,
-#define NexBACKCOLOR0   "bco0"   //     ,       ,       ,            ,        ,     ,      ,         ,       ,      ,         , DualStateButton
-#define NexBACKCOLOR1   "bco1"   //     ,       ,       ,            ,        ,     ,      ,         ,       ,      ,         , DualStateButton
-#define NexBACKCOLORACT "bco2"   //     ,       , Button,            ,        ,     ,      ,         ,       ,      ,         ,
-#define NexFORECOLOR    "pco"    // Text, Number, Button, Progressbar,        ,     , Gauge,         , Slider,      ,         ,
-#define NexFORECOLOR0   "pco0"   //     ,       ,       ,            ,        ,     ,      , Waveform,       ,      ,         ,
-#define NexFORECOLOR1   "pco1"   //     ,       ,       ,            ,        ,     ,      , Waveform,       ,      ,         ,
-#define NexFORECOLOR2   "pco2"   //     ,       ,       ,            ,        ,     ,      , Waveform,       ,      ,         ,
-#define NexFORECOLOR3   "pco3"   //     ,       ,       ,            ,        ,     ,      , Waveform,       ,      ,         ,
-#define NexFORECOLORACT "pco2"   //     ,       , Button,            ,        ,     ,      ,         ,       ,      ,         ,
-#define NexPICTURE      "pic"    // Text, Number, Button,            , Picture,     ,      , Waveform, Slider,      ,         ,
-#define NexPICTURE0     "pic0"   //     ,       ,       ,            ,        ,     ,      ,         ,       ,      ,         , DualStateButton
-#define NexPICTURE1     "pic1"   //     ,       ,       ,            ,        ,     ,      ,         ,       ,      ,         , DualStateButton
-#define NexPICTUREACT   "pic2"   // Text, Number, Button,            ,        ,     ,      ,         , Slider,      ,         ,
-#define NexCROPIMAGE    "picc"   // Text, Number, Button,            ,        , Crop, Gauge, Waveform, Slider,      ,         ,
-#define NexCROPIMAGE0   "picc0"  //     ,       ,       ,            ,        ,     ,      ,         ,       ,      ,         , DualStateButton
-#define NexCROPIMAGE1   "picc1"  //     ,       ,       ,            ,        ,     ,      ,         ,       ,      ,         , DualStateButton
-#define NexCROPIMAGEACT "picc2"  // Text, Number, Button,            ,        ,     ,      ,         ,       ,      ,         ,
-#define NexBACKPICTURE  "bpic"   //     ,       ,       , Progressbar,        ,     ,      ,         ,       ,      ,         ,
-#define NexFOREPICTURE  "ppic"   //     ,       ,       , Progressbar,        ,     ,      ,         ,       ,      ,         ,
-#define NexFONT         "font"   // Text, Number, Button,            ,        ,     ,      ,         ,       ,      ,         ,
-#define NexCENTERX      "xcen"   // Text, Number, Button,            ,        ,     ,      ,         ,       ,      ,         ,
-#define NexCENTERY      "ycen"   // Text, Number, Button,            ,        ,     ,      ,         ,       ,      ,         ,
-#define NexTEXT         "txt"    // Text,       , Button,            ,        ,     ,      ,         ,       ,      , Variable,
-#define NexVALUE        "val"    //     , Number,       , Progressbar,        ,     , Gauge,         , Slider,      , Variable, DualStateButton
-#define NexMINVALUE     "minval" //     ,       ,       ,            ,        ,     ,      ,         , Slider,      ,         , 
-#define NexMAXVALUE     "maxval" //     ,       ,       ,            ,        ,     ,      ,         , Slider,      ,         , 
-#define NexLENGTH       "lenth"  //     , Number,       ,            ,        ,     ,      ,         ,       ,      ,         , 
-#define NexWIDTH        "wid"    //     ,       ,       ,            ,        ,     , Gauge,         ,       ,      ,         , 
-#define NexGRIDCOLOR    "gdc"    //     ,       ,       ,            ,        ,     ,      , Waveform,       ,      ,         , 
-#define NexGRIDWIDTH    "gdw"    //     ,       ,       ,            ,        ,     ,      , Waveform,       ,      ,         , 
-#define NexTHUMBWIDTH   "wid"    //     ,       ,       ,            ,        ,     ,      ,         , Slider,      ,         , 
-#define NexTHUMBHEIGHT  "hig"    //     ,       ,       ,            ,        ,     ,      ,         , Slider,      ,         , 
-#define NexINTERVAL     "tim"    //     ,       ,       ,            ,        ,     ,      ,         ,       , Timer,         , 
-#define NexENABLED      "en"     //     ,       ,       ,            ,        ,     ,      ,         ,       , Timer,         , 
 
 #if defined(SPARK)
 #include "application.h"
